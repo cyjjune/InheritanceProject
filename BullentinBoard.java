1f99840ae4f2abd149b882dd3345531f028a0ebc
cyjjune <yunnneltmt@gmail.com>
2016-04-11 21:01:58 -0400

    Create BullentinBoard.java

---
+++ BullentinBoard.java
@@ -0,0 +1,12 @@
+public class BullentinBoard{
+  
+  public static void main(String[] args){
+    Club Photography = new Club( "B303", "Ms.D", 1540, 60 );
+    Photography.intro();
+    
+    Sports Basketball = new Sports( "Basketball Court", "Mr.O", 1540, 60, Sports.Balls.Basketball);
+    Basketball.intro();
+  }
+  
+  
+}
